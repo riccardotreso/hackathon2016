1611dabd1a8c26e67506f39861e879fa850a221e
christianderedita <christian.deredita@gmail.com>
2016-03-12 13:58:28 +0100

    Upload

---
+++ NTTTube/NTTTube.Web/Controllers/UploadController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NTTTube.Web.Controllers
+{
+    public class UploadController : Controller
+    {
+        //
+        // GET: /Upload/
+
+        public ActionResult UploadDocument()
+        {
+            return View("~/Views/Upload.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase FileUpload)
+        {
+            System.Threading.Thread.Sleep(5000);
+
+            if (FileUpload != null && FileUpload.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(FileUpload.FileName);
+                var path = Path.Combine(Server.MapPath("~/Video/"), fileName);
+                FileUpload.SaveAs(path);
+            }
+
+            // FTP Async
+
+
+            return RedirectToAction("UploadDocument");
+        }
+
+    }
+}
