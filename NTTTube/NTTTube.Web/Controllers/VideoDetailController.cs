71ac6bbf120c2889c55cc84d7e105887826b596f
alebravo73 <alebravo73@hotmail.com>
2016-03-12 14:02:56 +0100

    ale_link

---
+++ NTTTube/NTTTube.Web/Controllers/VideoDetailController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NTTTube.Web.Controllers
+{
+    public class VideoDetailController : Controller
+    {
+        //
+        // GET: /VideoDetail/
+
+        public ActionResult VideoDetailDocument()
+        {
+            return View("~/Views/VideoDetail.cshtml");
+        }
+
+    }
+}
