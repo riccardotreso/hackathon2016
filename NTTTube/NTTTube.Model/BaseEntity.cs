993d119047d337132b828fec39bb1fa44679d55f
christianderedita <christian.deredita@gmail.com>
2016-03-12 01:25:23 +0100

    Non aveva committato aluni file che merda....

---
+++ NTTTube/NTTTube.Model/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NTTTube.Model
+{
+    public class BaseEntity
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+}
---
+++ NTTTube/NTTTube.Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTTTube.Model
+{
+    public class User : BaseEntity
+    {
+        public string name { get; set; }
+        
+        
+    }
+}
